d23cd1e525007768e5eed46c46775cdf72072559
Andy Koch <akandrewkoch@gmail.com>
2020-09-07 16:40:31 -0500

    -fixed runshare capitalization on home page

--- src/main/java/com/akandrewkoch/mavenRunshare/controllers/HomeController.java
+++ src/main/java/com/akandrewkoch/mavenRunshare/controllers/HomeController.java
@@ -2,12 +2,17 @@ package com.akandrewkoch.mavenRunshare.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
+@RequestMapping("")
 public class HomeController extends MainController {
+
+    @GetMapping
     private String displayIndexPage(Model model, HttpServletRequest request){
         setRunnerInModel(request, model);
         model.addAttribute("title", "Home");
